648ff509ef0c7603d7b181f8cdfaee51c4d4e2f0
Oyawe <oyawegoodness@gmail.com>
2024-07-21 07:23:25 -0700

    framer motion implementation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,56 +1,69 @@
 import video1 from "../assets/video1.mp4";
 import video2 from "../assets/video2.mp4";
+import { motion } from "framer-motion";
+import { staggerContainer, textVariant } from "../utils/motion";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center mt-6 lg:mt-20 ">
-      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
-        VirtualR build tools
-        <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
-          {" "}
-          for developers
-        </span>
-      </h1>
-      <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
-        Empower your creativity and bring your VR app ideas to life with our
-        intuitive development tools. Get started today and turn your imagination
-        into immersive reality!
-      </p>
-      <div className="flex justify-center my-10">
-        <a
-          href="#"
-          className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md"
-        >
-          Start for free
-        </a>
-        <a
-          href="#"
-          className="py-3 px-4 mx-3 border hover:border-orange-500 ease-in duration-150 rounded-md"
-        >
-          Documentation
-        </a>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+    >
+      <div className="flex flex-col items-center mt-6 lg:mt-20 ">
+        <motion.h1 variants={textVariant(1.1)}>
+          <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+            VirtualR build tools
+            <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
+              {" "}
+              for developers
+            </span>
+          </h1>
+        </motion.h1>
+        <motion.p variants={textVariant(1.2)}>
+          <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
+            Empower your creativity and bring your VR app ideas to life with our
+            intuitive development tools. Get started today and turn your
+            imagination into immersive reality!
+          </p>
+        </motion.p>
+        <div className="flex justify-center my-10">
+          <a
+            href="#"
+            className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md"
+          >
+            Start for free
+          </a>
+          <a
+            href="#"
+            className="py-3 px-4 mx-3 border hover:border-orange-500 ease-in duration-150 rounded-md"
+          >
+            Documentation
+          </a>
+        </div>
+        <div className="flex flex-col lg:flex-row mt-10 justify-center">
+          <video
+            autoPlay
+            loop
+            muted
+            className="relative right-2 rounded-lg w-full lg:w-1/2 border border-orange-700 shadow shadow-orange-400 mx-2 my-4 "
+          >
+            <source src={video2} type="video/mp4" />
+            Your browser does not support this video.
+          </video>
+          <video
+            autoPlay
+            loop
+            muted
+            className="relative right-2 rounded-lg w-full lg:w-1/2 border border-orange-700 shadow shadow-orange-400 mx-2 my-4 "
+          >
+            <source src={video1} type="video/mp4" />
+            Your browser does not support this video.
+          </video>
+        </div>
       </div>
-      <div className="flex flex-col lg:flex-row mt-10 justify-center">
-        <video
-          autoPlay
-          loop
-          muted
-          className="relative right-2 rounded-lg w-full lg:w-1/2 border border-orange-700 shadow shadow-orange-400 mx-2 my-4 "
-        >
-          <source src={video2} type="video/mp4" />
-          Your browser does not support this video.
-        </video>
-        <video
-          autoPlay
-          loop
-          muted
-          className="relative right-2 rounded-lg w-full lg:w-1/2 border border-orange-700 shadow shadow-orange-400 mx-2 my-4 "
-        >
-          <source src={video1} type="video/mp4" />
-          Your browser does not support this video.
-        </video>
-      </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Menu, X } from "lucide-react";
 import logo from "../assets/logo.png";
 import { navItems } from "../constants";
+import { motion } from "framer-motion";
+import { navVariants } from "../utils/motion";
 import { useState } from "react";
 
 const Navbar = () => {
@@ -10,71 +12,73 @@ const Navbar = () => {
     setMenux(!menux);
   };
   return (
-    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
-      <div className="container px-4 mx-auto relative text-sm">
-        <div className="flex justify-between items-center">
-          {/* nav logo */}
-          <div className="flex items-center flex-shrink-0">
-            <img className="h-10 w-10 mr-2" src={logo} alt="logo" />
-            <span className="text-xl tracking-tight">VirtualR</span>
-          </div>
-          {/* nav links */}
-          <ul className="hidden lg:flex ml-14 space-x-12 tracking-tight">
-            {navItems.map((item, index) => (
-              <li key={index}>
-                <a
-                  href={item.href}
-                  className="hover:text-orange-500 ease-in duration-150"
-                >
-                  {item.label}
-                </a>
-              </li>
-            ))}
-          </ul>
-          {/* other nav links */}
-          <div className="hidden lg:flex justify-center space-x-12 items-center">
-            <a
-              href="#"
-              className="py-2 px-3 border rounded-md hover:border-orange-500 ease-in duration-150"
-            >
-              Sign In
-            </a>
-            <a
-              href="#"
-              className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
-            >
-              Create an account
-            </a>
-          </div>
-          {/* mobile menu  */}
-          <div className="lg:hidden md:flex flex-col justify-end">
-            <button onClick={toggleNavbar}>{menux ? <X /> : <Menu />}</button>
-          </div>
-        </div>
-        {menux && (
-          <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden sidebar ">
-            <ul>
+    <motion.nav variants={navVariants} initial="hidden" whileInView="show">
+      <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+        <div className="container px-4 mx-auto relative text-sm">
+          <div className="flex justify-between items-center">
+            {/* nav logo */}
+            <div className="flex items-center flex-shrink-0">
+              <img className="h-10 w-10 mr-2" src={logo} alt="logo" />
+              <span className="text-xl tracking-tight">VirtualR</span>
+            </div>
+            {/* nav links */}
+            <ul className="hidden lg:flex ml-14 space-x-12 tracking-tight">
               {navItems.map((item, index) => (
-                <li key={index} className="py-3 text-center">
-                  <a href={item.href}>{item.label}</a>
+                <li key={index}>
+                  <a
+                    href={item.href}
+                    className="hover:text-orange-500 ease-in duration-150"
+                  >
+                    {item.label}
+                  </a>
                 </li>
               ))}
             </ul>
-            <div className="flex flex-col">
-              <a href="#" className="py-2 px-3 text-center rounded-md border">
+            {/* other nav links */}
+            <div className="hidden lg:flex justify-center space-x-12 items-center">
+              <a
+                href="#"
+                className="py-2 px-3 border rounded-md hover:border-orange-500 ease-in duration-150"
+              >
                 Sign In
               </a>
               <a
                 href="#"
-                className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 mt-4 rounded-md"
+                className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
               >
                 Create an account
               </a>
             </div>
+            {/* mobile menu  */}
+            <div className="lg:hidden md:flex flex-col justify-end">
+              <button onClick={toggleNavbar}>{menux ? <X /> : <Menu />}</button>
+            </div>
           </div>
-        )}
-      </div>
-    </nav>
+          {menux && (
+            <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden sidebar ">
+              <ul>
+                {navItems.map((item, index) => (
+                  <li key={index} className="py-3 text-center">
+                    <a href={item.href}>{item.label}</a>
+                  </li>
+                ))}
+              </ul>
+              <div className="flex flex-col">
+                <a href="#" className="py-2 px-3 text-center rounded-md border">
+                  Sign In
+                </a>
+                <a
+                  href="#"
+                  className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 mt-4 rounded-md"
+                >
+                  Create an account
+                </a>
+              </div>
+            </div>
+          )}
+        </div>
+      </nav>
+    </motion.nav>
   );
 };
 
